53290bda6f5c26f80730d060290fa441d003bee3
Heejae <Heejae>
2016-06-10 19:57:10 +0900

    final

--- TeamProject/pingpong2p.cpp
+++ TeamProject/pingpong2p.cpp
@@ -2,318 +2,342 @@
 #include<stdlib.h>
 #include<time.h>
 #include<string.h>
-#include<GL/glut.h>
+#include<glut.h>
+#include <math.h>
+
 using namespace std;
- 
-double paddle1x, paddle1y;
-double paddle2x, paddle2y;
-double ballx, bally, balldirx, balldiry;
 
-GLfloat vertices[8][3]={{-10,-30,10}, {-10,30,10},
-			 			 {10,30,10},   {10,-30,10},
-			 			 {-10,-30,-10},{-10,30,-10},
-			 			 {10,30,-10},  {10,-30,-10}};
-GLfloat colors[8][3]={{1.0,0.0,0.0},{1.0,0.0,0.0},
-				  {1.0,0.0,0.0},{1.0,0.0,0.0},
-				  {0.0,0.0,1.0},{0.0,0.0,1.0},
-				  {0.0,0.0,1.0},{0.0,0.0,1.0}};
-GLubyte cubeIndices[24] = {0,3,2,1,
-					   	   2,3,7,6,
-    					   0,4,7,3,
-						   1,2,6,5,
-						   4,5,6,7,
-						   0,1,5,4};
+int state = 1; //게임모드. 2이면 3d게임모드
+double paddle1x, paddle1y; //player1의 패들좌표
+double paddle2x, paddle2y; //player2의 패들좌표
+double ballx, bally;//공의 좌표
+double balldirx, balldiry; //공의 방향
+double theta = 0;//각도
 
-int size_up = 3; 
-int flag = 0;
+GLfloat vertices[8][3] = { { -10, -30, 10 },{ -10, 30, 10 },
+{ 10, 30, 10 },{ 10, -30, 10 },
+{ -10, -30, -10 },{ -10, 30, -10 },
+{ 10, 30, -10 },{ 10, -30, -10 } };
+GLfloat colors1[8][3] = { { 1.0, 0.0, 0.0 },{ 1.0, 0.0, 0.0 },
+{ 1.0, 0.0, 0.0 },{ 1.0, 0.0, 0.0 },
+{ 0.0, 0.0, 1.0 },{ 0.0, 0.0, 1.0 },
+{ 0.0, 0.0, 1.0 },{ 0.0, 0.0, 1.0 } };
+GLfloat colors2[8][3] = { { 0.0, 1.0, 0.0 },{ 0.0, 1.0, 0.0 },
+{ 0.0, 1.0, 0.0 },{ 0.0, 1.0, 0.0 },
+{ 1.0, 1.0, 0.0 },{ 1.0, 1.0, 0.0 },
+{ 1.0, 1.0, 0.0 },{ 1.0, 1.0, 0.0 } };
+GLubyte cubeIndices[24] = { 0, 3, 2, 1,
+2, 3, 7, 6,
+0, 4, 7, 3,
+1, 2, 6, 5,
+4, 5, 6, 7,
+0, 1, 5, 4 };
 
- 
-int player1score, player2score;
-char buffer[10];
- 
+int player1score, player2score;//플레이어 스코어
+char buffer[10];//스코어 받는 버퍼
+
 bool keystates[256];
- 
+
+typedef struct materialStruct {
+	GLfloat ambient[4];
+	GLfloat diffuse[4];
+	GLfloat specular[4];
+	GLfloat shininess;
+}materialStruct;
+
+materialStruct ball = { 1.0, 0.2, 0.2, 1.0,
+1.0, 0.2, 0.2, 1.0,
+1.0, 0.2, 0.2, 1.0, 50.0 };
+
+void materials(materialStruct *material) {
+	glMaterialfv(GL_FRONT, GL_AMBIENT, material->ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, material->diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, material->specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, material->shininess);
+}
+
+void setLighting() {
+	GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat light_specular[] = { 1.0, 1.0, 1.0, 0.0 };
+	GLfloat light_pos[] = { -1.0, 0.0, 0.0, 0.0 };
+
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+
+	glShadeModel(GL_SMOOTH);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+}
+
+/*변수 초기화*/
 void initgame()
 {
-   ballx = 300-320;
-   bally = 400-240;
-   paddle1x = 50-320;
-   paddle1y = 240-240;
-   paddle2x = 590-320;
-   paddle2y = 240-240;
-
-   balldirx = 5;
-   balldiry = 5;
+	ballx = -20;
+	bally = 160;
+	paddle1x = -270;
+	paddle1y = 30;
+	paddle2x = 270;
+	paddle2y = 0;
+	balldirx = 5;
+	balldiry = 5;
 }
 
-void KeyUpPress(unsigned char key, int x, int y ){
-   switch(key){
-   case 'w':
-      keystates['w'] = false;
-      break;
-   case 's':
-      keystates['s'] = false;
-      break;
-   case '8':
-      keystates['8'] = false;
-      break;
-   case '2':
-      keystates['2'] = false;
-      break;
-   case 'c':
-      break;
-   case 27:
-      exit(0);
-
-   } 
+/*키보드up_press*/
+void KeyUpPress(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'w':
+		keystates['w'] = false;
+		break;
+	case 's':
+		keystates['s'] = false;
+		break;
+	case 'o':
+		keystates['o'] = false;
+		break;
+	case 'k':
+		keystates['k'] = false;
+		break;
+	case 'c':
+		break;
+	case 27:
+		exit(0);
+	}
 }
- 
-void KeyDownPress(unsigned char key, int x, int y ){
-    switch(key){
-   case 'w':
-      keystates['w'] = true;
-   break;
- 
-   case 's':
-      keystates['s'] = true;
-      break;
- 
-   case '8':
-      keystates['8'] = true;
-      break;
-   
-   case '2':
-      keystates['2'] = true;
-      break;
-   case 'c':
-      break;
-   case 27:
-      exit(0);
- 
-   }
-      
-}
- 
-void drawString (void * font, char *s, float x, float y){
-     unsigned int i;
-     glRasterPos2f(x, y);
- 
-     for (i = 0; i < strlen (s); i++)
-          glutBitmapCharacter (font, s[i]);
+/*키보드down_press*/
+void KeyDownPress(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'w':
+		keystates['w'] = true;
+		break;
+	case 's':
+		keystates['s'] = true;
+		break;
+	case 'o':
+		keystates['o'] = true;
+		break;
+	case 'k':
+		keystates['k'] = true;
+		break;
+	case 'c':
+		break;
+	case 27:
+		exit(0);
+	}
 }
 
+/*string 화면에 출력*/
+void drawString(void * font, char *s, float x, float y) {
+	unsigned int i;
+	glRasterPos2f(x, y);
+
+	for (i = 0; i < strlen(s); i++)
+		glutBitmapCharacter(font, s[i]);
+}
 
 
+/*player1의 패들그리기*/
 void drawpaddle1()
 {
+	if (state == 2)
+		glRotated(theta, 0, 1, 0);
 	glTranslatef(-280, paddle1y, 0);
-	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, cubeIndices);
-	//glLoadIdentity();
 
-	/*
-   glBegin(GL_POLYGON);
-      glColor3f(1, 1, 1);
-      glVertex3f (paddle1x - 10, paddle1y - 30,0);
-        glVertex3f (paddle1x - 10, paddle1y + 30,0);
-        glVertex3f (paddle1x + 10, paddle1y + 30,0);
-        glVertex3f (paddle1x + 10, paddle1y - 30,0);
-   glEnd();*/
+	glVertexPointer(3, GL_FLOAT, 0, vertices);
+	glColorPointer(3, GL_FLOAT, 0, colors1);
+	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, cubeIndices);
+	glFlush();
 }
- 
+
+/*player1의 패들그리기*/
 void drawpaddle2()
 {
+	glLoadIdentity();
+
+	if (state == 2)
+		glRotated(theta, 0, 1, 0);
 	glTranslatef(280, paddle2y, 0);
+
+	glVertexPointer(3, GL_FLOAT, 0, vertices);
+	glColorPointer(3, GL_FLOAT, 0, colors2);
 	glDrawElements(GL_QUADS, 24, GL_UNSIGNED_BYTE, cubeIndices);
-	//glLoadIdentity();
-	/*
-   glBegin(GL_POLYGON);
-      glColor3f(1, 1, 1);
-      glVertex3f (paddle2x - 10, paddle2y - 30,0);
-        glVertex3f (paddle2x - 10, paddle2y + 30,0);
-        glVertex3f (paddle2x + 10, paddle2y + 30,0);
-        glVertex3f (paddle2x + 10, paddle2y - 30,0);
-   glEnd();*/
+
+	glFlush();
+
+
 }
- 
+
+/*공의 움직임 계산*/
 void moveball()
 {
-   ballx = ballx + balldirx;
- 
-   //check if the ball hit the wall (좌우)
-   if(ballx + 5 > 320 || ballx - 5 < -320)   //if(ballx + 5 > 640 || ballx - 5 < 0)
-   {
-      if(ballx + 5 > 320)
-         player1score = player1score + 1;
-      if(ballx - 5 < -320)
-         player2score = player2score + 1;
-      balldirx = -balldirx;
-   }   
- 
-   //check if the ball hit the wall(상하)
-   bally = bally + balldiry;
-   if(bally + 5 > 240 || bally - 5 < -240)
-      balldiry = -balldiry;
- 
-   //check if the ball hit the paddle
-   if(balldirx >= 1)
-   {
-      if(ballx + 5 > paddle2x - 10 && ballx - 5 < paddle2x + 10 && bally - 5 < paddle2y + 30 && bally + 5 > paddle2y - 30)
-      {
-         balldirx = -balldirx;
-      }
-   }
-   else
-   {
-      if(ballx + 5 > paddle1x - 10 && ballx - 5 < paddle1x + 10 && bally - 5 < paddle1y + 30 && bally + 5 > paddle1y - 30)
-      {
-         balldirx = -balldirx;
-      }
-   }
+	ballx = ballx + balldirx;
+
+	//check if the ball hit the wall (좌우)
+	if (ballx + 5 > 320 || ballx - 5 < -320)
+	{
+		if (ballx + 5 > 320)
+			player1score = player1score + 1;
+		if (ballx - 5 < -320)
+			player2score = player2score + 1;
+		balldirx = -balldirx;
+	}
+
+	//check if the ball hit the wall(상하)
+	bally = bally + balldiry;
+	if (bally + 5 > 240 || bally - 5 < -240)
+		balldiry = -balldiry;
+
+	//check if the ball hit the paddle
+	if (balldirx >= 1)
+	{
+		if (ballx + 5 > paddle2x - 10 && ballx - 5 < paddle2x + 10
+			&& bally - 5 < paddle2y + 30 && bally + 5 > paddle2y - 30)
+		{
+			balldirx = -balldirx;
+		}
+	}
+	else
+	{
+		if (ballx + 5 > paddle1x - 10 && ballx - 5 < paddle1x + 10
+			&& bally - 5 < paddle1y + 30 && bally + 5 > paddle1y - 30)
+		{
+			balldirx = -balldirx;
+		}
+	}
 }
- 
+
+/*공그리기*/
 void drawball()
 {
-   if((player1score>3 || player2score>3)  & flag ==0)//수정한거
-      {size_up +=7;//수정한거
-   flag =1;}//수정한거
+	materialStruct *matp;
 
-   glTranslatef(ballx,bally,0);         
-   glutSolidSphere(5,32,32);
-   glFlush();
-   //glBegin(GL_POLYGON);
-     // glColor3f(1, 1, 1);
-      /*glVertex2f (ballx - 5, bally - 5);
-        glVertex2f (ballx - 5, bally + 5);
-        glVertex2f (ballx + 5, bally + 5);
-        glVertex2f (ballx + 5, bally - 5);*/
+	glLoadIdentity();
+	if (state == 2)
+		glRotated(theta, 0, 1, 0);
+	glTranslatef(ballx, bally, 0);
 
-      /*glVertex3f (ballx - size_up, bally - size_up,0);
-        glVertex3f (ballx - size_up, bally + size_up,0);
-        glVertex3f (ballx + size_up, bally + size_up,0);
-        glVertex3f (ballx + size_up, bally - size_up,0);*/
+	matp = &ball;
+	materials(matp);
+	glColor3d(1.0, 0.0, 0.0);
+	glutSolidSphere(10, 32, 32);
 
-  // glEnd();
+	glFlush();
 }
- 
-char *scoreformat(char *score, int size)
+
+void myTimer(int v)
 {
-   char *formattedscore = new char[size];
-   int scorelength;
-   int k = 0;
- 
-   scorelength = strlen(score);
- 
-   for(int i = 0; i <= size - 1; i++)
-   {
-      if(i <= (size - 1) - scorelength)
-      {
-         formattedscore[i] = '0';
-      }
-      else
-      {
-         formattedscore[i] = score[k++];
-      }
-   }
-   return formattedscore;
+	theta += 0.2;
+	if (theta >= 360.0) theta -= 360.0;
+
+	glutPostRedisplay();
+	glutTimerFunc(10, myTimer, v);
 }
- 
+
 
 void display()
 {
-   glMatrixMode(GL_MODELVIEW);   
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);    
-   glLoadIdentity();   
-   gluLookAt(0,0,1, 0,0,0, 0,1,0);
+	glMatrixMode(GL_MODELVIEW);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt(0, 0, 1, 0, 0, 0, 0, 1, 0);
 
-   if(player1score>10 || player2score>10)
-   {      
-      if(player1score > player2score)
-      {
-         glutSwapBuffers();
-         glClear(GL_COLOR_BUFFER_BIT);
-         drawString(GLUT_BITMAP_HELVETICA_18, "player1 win!!!!!!!!!!!", 300, 240);
-      }
-      else
-      {
-         glutSwapBuffers();
-         glClear(GL_COLOR_BUFFER_BIT);
-         drawString(GLUT_BITMAP_HELVETICA_18, "player2 win!!!!!!!!!!!", 300, 240);
-      }
-   }
+	/*10점이상 점수를 내면 게임 끝*/
+	if (player1score >= 10 || player2score >= 10)
+	{
+		return;
+	}
+	else
+	{
+		/*점수 출력*/
+		glColor3f(1, 1, 1);
+		drawString(GLUT_BITMAP_HELVETICA_18, "Player1:", -310, -220);
+		itoa(player1score, buffer, 10);
+		drawString(GLUT_BITMAP_HELVETICA_18, buffer, -310, -200);
 
-   else{
-   //////////////////////////////////
-   glClear(GL_COLOR_BUFFER_BIT);
+		drawString(GLUT_BITMAP_HELVETICA_18, "Player2:", 240, -220);
+		itoa(player2score, buffer, 10);
+		drawString(GLUT_BITMAP_HELVETICA_18, buffer, 240, -200);
 
+		/*"s","w"키로 플레이어1 패들의 상하 움직임*/
+		if (keystates['s'] == true)//키보드 누르면
+			if (paddle1y - 30 > -240)
+				paddle1y = paddle1y - 5;
+		if (keystates['w'] == true)
+			if (paddle1y + 30 < 240)
+				paddle1y = paddle1y + 5;
+		drawpaddle1();
 
-   drawString(GLUT_BITMAP_HELVETICA_18, "Player1:", -310, -220);
-   itoa(player1score, buffer, 10);
-   drawString(GLUT_BITMAP_HELVETICA_18, buffer, -310, -200);
- 
-   drawString(GLUT_BITMAP_HELVETICA_18, "Player2:", 240, -220);
-   itoa(player2score, buffer, 10);
-   drawString(GLUT_BITMAP_HELVETICA_18, buffer, 240, -200);
- 
-   if(keystates['s'] == true)
-      if(paddle1y - 30 > -240)
-         paddle1y = paddle1y - 5;
-   if(keystates['w'] == true)
-      if(paddle1y + 30 < 240)
-         paddle1y = paddle1y + 5;
-   drawpaddle1();
+		/*"k","o"키로 플레이어2 패들의 상하 움직임*/
+		if (keystates['k'] == true)
+			if (paddle2y - 30 > -240)
+				paddle2y = paddle2y - 5;
+		if (keystates['o'] == true)
+			if (paddle2y + 30 < 240)
+				paddle2y = paddle2y + 5;
 
- 
-   if(keystates['8'] == true)
-      if(paddle2y - 30 > -240)
-         paddle2y = paddle2y - 5;
-   if(keystates['2'] == true)
-      if(paddle2y + 30 < 240)
-         paddle2y = paddle2y + 5;
-   drawpaddle2();
-   
-   moveball();
+		setLighting();
+		moveball();
+		drawball();
 
-   drawball();
- 
-   glutSwapBuffers();
-}
+		glDisable(GL_LIGHTING);
+		drawpaddle2();
+
+		glutSwapBuffers();
+	}
 
 }
- 
+
+void demo_menu(int id)
+{
+	switch (id)
+	{
+	case 1: state = 1; break;
+	case 2: state = 2; break;
+	}
+	glutPostRedisplay();
+}
+
 void reshape(int x, int y)
 {
-   glViewport(0, 0, x, y);
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   //glOrtho(0, 640, 0, 480, -10, 10);
-   glOrtho(-320, 320, -240, 240, -200, 200);
-   glMatrixMode(GL_MODELVIEW);
+	glViewport(0, 0, x, y);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-320, 320, -240, 240, -320, 320);
+	glMatrixMode(GL_MODELVIEW);
 }
- 
+
 void main(int argc, char **argv)
 {
-   initgame();
-   glutInit(&argc,argv);
-   glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
-   glutInitWindowPosition(100,100);
-   glutInitWindowSize(640,480);
-   glutCreateWindow("Pong");
-   glutReshapeFunc(reshape);
-   glutDisplayFunc(display);
-   //glutIgnoreKeyRepeat(1);
-   glutKeyboardFunc(KeyDownPress);
-   //glutKeyboardUpFunc(KeyUpPress);
-   glutIdleFunc(display);
-
-   /*입방체를 위한 코드*/
-   glEnableClientState(GL_COLOR_ARRAY);
+	initgame();
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(640, 480);
+	glutCreateWindow("Pong");
+	glutReshapeFunc(reshape);
+	glutDisplayFunc(display);
+	glutIgnoreKeyRepeat(1);
+	glutKeyboardFunc(KeyDownPress);
+	glutKeyboardUpFunc(KeyUpPress);
+	/*팝업메뉴를 위한 코드*/
+	glutCreateMenu(demo_menu);
+	glutAddMenuEntry("2d게임모드", 1);
+	glutAddMenuEntry("3d게임모드", 2);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+	/*입방체를 위한 코드*/
+	glEnableClientState(GL_COLOR_ARRAY);
 	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 0, vertices);
-	glColorPointer(3, GL_FLOAT, 0, colors);
 
-   glutFullScreen();   
-   glEnable(GL_DEPTH_TEST);
-   glutMainLoop();
+	glutTimerFunc(100, myTimer, 1);
 
-
+	//glutFullScreen();   
+	glEnable(GL_DEPTH_TEST);
+	glutMainLoop();
 }
